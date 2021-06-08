d5ee9c016895fce06ee6339ae9a2755174ef1066
Eduard Ciugulea <ciugulea@yahoo.com>
2021-06-08 17:03:18 +0100

    lots of things moddified with ife

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,7 +12,6 @@ class Product < ApplicationRecord
   has_many :carts, through: :cart_products
   has_many :order_products
   has_many :orders, through: :order_products
-  has_many :reviews, through: :orders
   has_many_attached :photos
   validates :name, presence: true
   validates :description, presence: true

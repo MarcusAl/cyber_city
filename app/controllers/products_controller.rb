43b347947bc4e6a769b00242a55a9450c84fbc56
spencer ryton <spencerryton@gmail.com>
2021-06-08 16:30:53 +0100

    product controller and product views

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,41 @@
+class ProductsController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: [ :index ]
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user = current_user
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = "Your product '#{@product.name}' has been deleted."
+    redirect_to products_path
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :category, :photos)
+  end
+
+end
+

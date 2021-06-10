39549bc95289c28102b53d6d9851cf4f08ae1e81
Felix Altenburg <felix.n.altenburg@gmail.com>
2021-06-10 16:33:08 +0100

    implemented api

--- app/controllers/diagnostics_controller.rb
+++ app/controllers/diagnostics_controller.rb
@@ -1,6 +1,6 @@
-require 'json'
+require 'uri'
 require 'net/http'
-require 'time'
+require 'openssl'
 
 class DiagnosticsController < ApplicationController
   def index
@@ -20,12 +20,18 @@ class DiagnosticsController < ApplicationController
     case response
     when Net::HTTPOK then
       info = JSON.parse(response.body)
-      @test_id = info['job_id']
-      p @test_id
-      checker
+      p info
+      if info['test_id']
+        @test_id = info['test_id']
+        cached_checker
+      else
+        @test_id = info['job_id']
+        checker
+      end
     else
-      response.value
+      flash[:notice] = "Something went wrong, please try again."
     end
+    raise
   end
 
   private
@@ -34,14 +40,86 @@ class DiagnosticsController < ApplicationController
     params.require(:diagnostic).permit(:url)
   end
 
+  def cached_checker
+    uri = URI("https://www.immuniweb.com/websec/api/v1/get_result/#{@timestamp}.html")
+    diagnostic = Net::HTTP.post_form(uri, 'id' => @test_id)
+    diagnostic_results = JSON.parse(diagnostic.body)
+    @diagnostics = {
+      score: diagnostic_results['score'],
+      grade: diagnostic_results['grade'],
+      tested_url: diagnostic_results['tested_url'],
+      firewalled: diagnostic_results['is_firewalled'],
+      pci_compliance: diagnostic_results['compliance_pci_dss'],
+      gdpr_compliance: diagnostic_results['compliance_gdpr'],
+      lat: diagnostic_results['lat'],
+      lng: diagnostic_results['lng'],
+      city: diagnostic_results['city'],
+      scores: {
+        http_headers: {
+          description: diagnostic_results['internals']['scores']['http_headers']['description'],
+          colour: diagnostic_results['internals']['scores']['http_headers']['class']
+        },
+        csp: {
+          description: diagnostic_results['internals']['scores']['csp']['description'],
+          colour: diagnostic_results['internals']['scores']['csp']['class']
+        },
+        gdpr: {
+          description: diagnostic_results['internals']['scores']['gdpr']['description'],
+          colour: diagnostic_results['internals']['scores']['gdpr']['class']
+        },
+        app_scan: {
+          description: diagnostic_results['internals']['scores']['app_scan']['description'],
+          colour: diagnostic_results['internals']['scores']['app_scan']['class']
+        },
+        pci_dss: {
+          description: diagnostic_results['internals']['scores']['pci_dss']['description'],
+          colour: diagnostic_results['internals']['scores']['pci_dss']['class']
+        }
+      }
+    }
+  end
+
   def checker
     uri = URI("https://www.immuniweb.com/websec/api/v1/get_result/#{@timestamp}.html")
-    @diagnostic = Net::HTTP.post_form(uri, 'job_id' => @test_id)
-    if @diagnostic.body['status'] == 'in_progress'
+    diagnostic = Net::HTTP.post_form(uri, 'job_id' => @test_id)
+    if diagnostic.body['status'] == 'in_progress'
       sleep 60
       checker
     else
-      p @diagnostic.body
+      diagnostic_results = JSON.parse(diagnostic.body)
+      @diagnostics = {
+        score: diagnostic_results['score'],
+        grade: diagnostic_results['grade'],
+        tested_url: diagnostic_results['tested_url'],
+        firewalled: diagnostic_results['is_firewalled'],
+        pci_compliance: diagnostic_results['compliance_pci_dss'],
+        gdpr_compliance: diagnostic_results['compliance_gdpr'],
+        lat: diagnostic_results['lat'],
+        lng: diagnostic_results['lng'],
+        city: diagnostic_results['city'],
+        scores: {
+          http_headers: {
+            description: diagnostic_results['internals']['scores']['http_headers']['description'],
+            colour: diagnostic_results['internals']['scores']['http_headers']['class']
+          },
+          csp: {
+            description: diagnostic_results['internals']['scores']['csp']['description'],
+            colour: diagnostic_results['internals']['scores']['csp']['class']
+          },
+          gdpr: {
+            description: diagnostic_results['internals']['scores']['gdpr']['description'],
+            colour: diagnostic_results['internals']['scores']['gdpr']['class']
+          },
+          app_scan: {
+            description: diagnostic_results['internals']['scores']['app_scan']['description'],
+            colour: diagnostic_results['internals']['scores']['app_scan']['class']
+          },
+          pci_dss: {
+            description: diagnostic_results['internals']['scores']['pci_dss']['description'],
+            colour: diagnostic_results['internals']['scores']['pci_dss']['class']
+          }
+        }
+      }
     end
   end
 end

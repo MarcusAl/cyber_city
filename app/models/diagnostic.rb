0bcc9c475ed42a14e35571c0358bb298cb385e29
Eduard Ciugulea <ciugulea@yahoo.com>
2021-06-11 12:06:52 +0100

    get rid of vaildation

--- app/models/diagnostic.rb
+++ app/models/diagnostic.rb
@@ -1,10 +1,3 @@
 class Diagnostic < ApplicationRecord
   belongs_to :user
-  validates :url, presence: true
-  validates :score, presence: true
-  validates :wss_test, presence: true
-  validates :ss_test, presence: true
-  validates :gdpr_comp_test, presence: true
-  validates :pci_dss_test, presence: true
-  validates :http_headers_test, presence: true
 end

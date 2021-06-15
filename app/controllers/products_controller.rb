29829b4856ad729b8db6d9b3192064eb30d0d696
Felix Altenburg <felix.n.altenburg@gmail.com>
2021-06-15 14:26:26 +0100

    added search function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,11 @@ class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
-    @products = Product.all
-
+    if params[:query].present?
+      @products = Product.search_by_name_and_description(params[:query])
+    else
+      @products = Product.all
+    end
   end
 
   def new
@@ -37,6 +40,5 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:name, :description, :price, :category, :photos)
   end
-
 end
 

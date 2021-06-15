1f9814b1f4e5e2c5bcf35ef3efcab0ab1c863a5d
Marcus Allen <emkay1993@gmail.com>
2021-06-15 18:10:16 +0100

    Fix anim

--- app/javascript/components/init_cards.js
+++ app/javascript/components/init_cards.js
@@ -11,7 +11,7 @@ export const initCards = () => {
 
   animateIn.fromTo('.overlay', 2, { scale:1.5 }, { xPercent:100, scale:1, transformOrigin: '0% 100%', ease: Power4.easeOut })
 
-  .from('.promo-block', 1, { scaleY:0, transformOrigin: 'bottom left' }, '-=2')
+  .from('.promo-block', 1, { scaleY:0, transformOrigin: 'bottom left' }, '-=1')
 
   .from('.fa-briefcase', 1.5, { autoAlpha: 0, x:300, y:100, ease: Elastic.easeOut }, '-=1')
 
@@ -26,7 +26,7 @@ export const initCards = () => {
   // Make scrollMagic Scene
 
   var scene = new ScrollMagic.Scene({
-    triggerElement: '.banner-image-container'
+    triggerElement: '.trigger1'
     // When we scroll to .cards then it will setTween/activate the animateIn
     // Make sure the correct scripts are imported in html or it won't work
   })
@@ -38,7 +38,7 @@ export const initCards = () => {
 
   animateIn2.fromTo('.newscontainer', 2, { scale:1.5 }, { xPercent:100, scale:1, transformOrigin: '0% 50% 100%', ease: Elastic.easeOut })
 
-  .from('.serviceBox', 1, { scaleY:0, transformOrigin: 'bottom left' }, '-=2')
+  .from('.serviceBox', 1, { scaleY:0, transformOrigin: 'bottom left', ease: Elastic.easeOut }, '-=3')
 
   // Make scrollMagic Scene
 

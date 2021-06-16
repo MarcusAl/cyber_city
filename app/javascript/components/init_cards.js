00639c5e15ab2ec785f4b871cae7eae944332be6
Marcus Allen <emkay1993@gmail.com>
2021-06-16 12:44:30 +0100

    Changes

--- app/javascript/components/init_cards.js
+++ app/javascript/components/init_cards.js
@@ -34,38 +34,38 @@ export const initCards = () => {
 
 
 
-  // New Scene
+  // // New Scene
 
-  animateIn2.fromTo('.newscontainer', 2, { scale:1.5 }, { xPercent:100, scale:1, transformOrigin: '0% 50% 100%', ease: Elastic.easeOut })
+  // animateIn2.fromTo('.newscontainer', 2, { scale:1.5 }, { xPercent:100, scale:1, transformOrigin: '0% 50% 100%', ease: Elastic.easeOut })
 
-  .from('.serviceBox', 1, { scaleY:0, transformOrigin: 'bottom left', ease: Elastic.easeOut }, '-=3')
+  // .from('.serviceBox', 1, { scaleY:0, transformOrigin: 'bottom left', ease: Elastic.easeOut }, '-=3')
 
-  // Make scrollMagic Scene
+  // // Make scrollMagic Scene
 
-  var scene2 = new ScrollMagic.Scene({
-    triggerElement: '.services'
-    // When we scroll to .cards then it will setTween/activate the animateIn
-    // Make sure the correct scripts are imported in html or it won't work
-  })
-  .setTween(animateIn2).addTo(controller2);
-
-
+  // var scene2 = new ScrollMagic.Scene({
+  //   triggerElement: '.services'
+  //   // When we scroll to .cards then it will setTween/activate the animateIn
+  //   // Make sure the correct scripts are imported in html or it won't work
+  // })
+  // .setTween(animateIn2).addTo(controller2);
 
 
 
-  // New Scene
-  // Animation Setup
-
-  animateIn3.fromTo('.overlay2', 2, { scale:1.2 }, { yPercent:2, xPercent:2, scale:1, transformOrigin: '0% 50% 100%', ease: Bounce.easeOut });
 
 
-  var scene3 = new ScrollMagic.Scene({
-    triggerElement: '.news-container',
-    triggerHook: "onEnter",
-    duration: '100%',
-    reverse: true
-    // When we scroll to .cards then it will setTween/activate the animateIn
-    // Make sure the correct scripts are imported in html or it won't work
-  })
-  .setTween(animateIn3).addTo(controller3);
+  // // New Scene
+  // // Animation Setup
+
+  // animateIn3.fromTo('.overlay2', 2, { scale:1.2 }, { yPercent:2, xPercent:2, scale:1, transformOrigin: '0% 50% 100%', ease: Bounce.easeOut });
+
+
+  // var scene3 = new ScrollMagic.Scene({
+  //   triggerElement: '.news-container',
+  //   triggerHook: "onEnter",
+  //   duration: '100%',
+  //   reverse: true
+  //   // When we scroll to .cards then it will setTween/activate the animateIn
+  //   // Make sure the correct scripts are imported in html or it won't work
+  // })
+  // .setTween(animateIn3).addTo(controller3);
 }
--- app/javascript/components/myHover.js
+++ app/javascript/components/myHover.js
@@ -1,13 +1,45 @@
 //Services Section
 
 export const myHover = () => {
-  TweenMax.set('.serviceBoxInner', { y: -100, opacity:0 });
+  // TweenMax.set('.serviceBoxInner', { y: -100, opacity:0 });
   
-  $('.serviceBox').hover(function() {
-      TweenMax.to($(this).find('.serviceBoxInner'), 0.9, { y: -150, x: -150, opacity:1, ease: Power2.easeOut });
-  },
-  function() {
-      TweenMax.to($(this).find('.serviceBoxInner'), 0.9, { y: 50, opacity:0 });
+  // $('.serviceBox').hover(function() {
+  //     TweenMax.to($(this).find('.serviceBoxInner'), 0.9, { y: -150, x: -150, opacity:1, ease: Power2.easeOut });
+  // },
+  // function() {
+  //     TweenMax.to($(this).find('.serviceBoxInner'), 0.9, { y: 50, opacity:0 });
+  // });
+
+  const slider = document.querySelector(".horizontal-timeline");
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  slider.addEventListener("mousedown", e => {
+    isDown = true;
+    slider.classList.add("active");
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+  slider.addEventListener("mouseleave", () => {
+    setTimeout(function() {
+        isDown = false;
+        slider.classList.remove("active");
+    }, 100);
+  });
+  slider.addEventListener("mouseup", () => {
+    setTimeout(function() {
+        isDown = false;
+        slider.classList.remove("active");
+    }, 100);
+  });
+  slider.addEventListener("mousemove", e => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(walk);
   });
 
 }
